aeb680630b67539537cd5524ed4615edf022c3ef
tanubordia <tb24697@gmail.com>
2020-04-03 10:14:05 +0530

    Update Seller.java

--- src/main/java/com/acms/CentralSellerPortal/Entities/Seller.java
+++ src/main/java/com/acms/CentralSellerPortal/Entities/Seller.java
@@ -1,4 +1,12 @@
 package com.acms.CentralSellerPortal.Entities;
 
 public class Seller {
+  int seller_id, seller_contactno;
+  String seller_name, shop_name, shop_address, password, email_id, pan, gstin;
+  public seller(int seller_id, String seller_name, String gstin)
+  {
+    this.seller_id= seller_id;
+    this.seller_name= seller_name;
+    this.gstin=gstin;
+}
 }
